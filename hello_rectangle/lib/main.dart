62ce8eea093f6dc85ddb0c7bf4583961f9015348
hyuraku <yu@hill-wood.com>
2019-03-19 22:52:09 +0900

    Container to MaterialApp

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
 //  print('Hello');
-  runApp(Container(
-    color: Colors.greenAccent,
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Container(
+      color: Colors.greenAccent,
+    ),
   ));
 }
