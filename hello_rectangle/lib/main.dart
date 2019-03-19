43f1d001a2b71167c1eb13936ae440e8be055ca7
hyuraku <yu@hill-wood.com>
2019-03-19 22:55:34 +0900

    add Scaffold

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
 //  print('Hello');
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Container(
-      color: Colors.greenAccent,
-    ),
-  ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('Hello rectangle')
+        ),
+        body: Container(
+          color: Colors.greenAccent,
+        ),
+      )));
 }
