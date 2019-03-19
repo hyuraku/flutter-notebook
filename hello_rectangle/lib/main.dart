11124199cb81e0e822de9d06864914c941f945be
hyuraku <yu@hill-wood.com>
2019-03-19 22:57:43 +0900

    make a widget

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
+Widget HelloRectangle() {
+  return Container(
+    color: Colors.greenAccent,
+  );
+}
+
 void main() {
 //  print('Hello');
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-            title: Text('Hello rectangle')
-        ),
-        body: Container(
-          color: Colors.greenAccent,
-        ),
+        appBar: AppBar(title: Text('Hello rectangle')),
+        body: HelloRectangle(),
       )));
 }
