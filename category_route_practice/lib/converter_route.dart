28caa57ea622f792644afc6a0df3afe2d662a899
hyuraku <yu@hill-wood.com>
2019-04-09 01:27:47 +0900

    make some method and widget

--- category_route_practice/lib/converter_route.dart
+++ category_route_practice/lib/converter_route.dart
@@ -27,7 +27,7 @@ class ConverterRoute extends StatefulWidget{
   _ConverterRoute createState() => _ConverterRoute();
 }
 class _ConverterRoute extends State<ConverterRoute> {
-  Unit _formValue;
+  Unit _fromValue;
   Unit _toValue;
   double _inputValue;
   String _convertValue = '';
@@ -63,6 +63,101 @@ class _ConverterRoute extends State<ConverterRoute> {
       _formValue = widget.units[0];
       _toValue = widget.units[1];
     });
+  }
+
+  String _format(double conversion){
+    var outputNum = conversion.toStringAsPrecision(7);
+    if(outputNum.contains('.') && outputNum.endsWith(0)){
+      var i = outputNum.length -1 ;
+      while(outputNum[i] == '0'){
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1)
+    }
+    if(outputNum.endsWith('.')){
+      return outputNum.substring(0, outputNum.length -1);
+    }
+    return outputNum;
+  }
+
+  void _updateConversion(){
+    setState(() {
+      _convertValue =
+          _format(_inputValue *(_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateInoutValues(String input){
+    setState(() {
+      if(input == null || input.isEmpty){
+        _convertValue = '';
+      }else{
+        try{
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateConversion();
+        } on Exception catch(e){
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
+  Unit _getUnit(String unitName){
+    return widget.units.firstWhere(
+        (Unit unit){
+          return unit.name == unitName;
+        },
+      orElse: null
+    );
+  }
+
+  void _updateFromConversion(dynamic unitName){
+    setState(() {
+      _fromValue = _getUnit(unitName);
+    });
+    if(_inputValue != null){
+      _updateConversion();
+    }
+  }
+
+  void _updateToConversion(dynamic unitName){
+    setState(() {
+      _toValue = _getUnit(unitName);
+    });
+    if(_inputValue != null){
+      _updateConversion();
+    }
+  }
+
+  Widget _createDropdown(String currentValue, ValueChanged<dynamic> onChanged){
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        border: Border.all(
+          color: Colors.grey[400],
+          width: 1.0,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.grey[50],
+          ),
+          child: DropdownButtonHideUnderline(
+              child: ButtonTheme(
+                alignedDropdown: true,
+                child: DropdownButton(
+                  value: currentValue,
+                  items: _unitMenuItems,
+                  onChanged: onChanged,
+                  style: Theme.of(context).textTheme.title,
+                )
+              ))),
+    );
   }
 
 
