335f8231b844c2fed63c3a496e5c13781421f645
hyuraku <yu@hill-wood.com>
2019-05-07 23:00:55 +0900

    create add item button

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -2,30 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new TodoApp());
 
-class TodoApp extends StatelessWidget{
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new MaterialApp(
-      title: 'Todo list',
-      home: new TodoList()
-    );
+    return new MaterialApp(title: 'Todo list', home: new TodoList());
   }
 }
 
-class TodoList extends StatefulWidget{
+class TodoList extends StatefulWidget {
   @override
   createState() => new TodoListState();
 }
 
-class TodoListState extends State<TodoList>{
+class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  void _addTodoItem() {
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item' + index.toString());
+    });
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(itemBuilder: (context, index) {
+      if (index < _todoItems.length) {
+        return _buildTodoItem(_todoItems[index]);
+      }
+    });
+  }
+
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(
+      title: new Text(todoText),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo List'),
-      )
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addTodoItem,
+        tooltip: 'Add task',
+        child: new Icon(Icons.add),
+      ),
     );
   }
 }
