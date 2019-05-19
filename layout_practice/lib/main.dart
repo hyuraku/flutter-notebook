eb4c34070ce7c3e6cdc60ed4d6658eaee87b33f1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-05-19 19:09:53 +0900

    add title-section

--- layout_practice/lib/main.dart
+++ layout_practice/lib/main.dart
@@ -5,6 +5,40 @@ void main() => runApp(myApp());
 class myApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Somewhere over the rake',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'you are not in Kansas any more',
+                  style: TextStyle(
+                    color: Colors.grey[500]
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41')
+        ],
+      ),
+    );
     // TODO: implement build
     return MaterialApp(
       title: "Flutter Layout memo",
@@ -12,8 +46,10 @@ class myApp extends StatelessWidget{
         appBar: AppBar(
           title: Text("Flutter Layout memo"),
         ),
-        body: Center(
-          child: Text("Hello world"),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
