e11bdf02a71b52bf97a0e74904cbf9954fe6d3dc
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-05-27 22:59:33 +0900

    create original screen

---
+++ layout_practice01/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget{
+ @override
+ Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "layout practice",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Layout Practice"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset(
+              'images/picture.jpg',
+              width: 600,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            detailSection
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget titleSection = Container(
+    padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+    child: Text(
+      'A Empty City',
+      style: TextStyle(
+          color: Colors.grey[500],
+        fontSize: 30.0
+      ),
+    ),
+  );
+  Widget detailSection = Container(
+    padding: const EdgeInsets.all(10),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the ',
+          softWrap: true,
+    ),
+  );
+
+}
+
+
