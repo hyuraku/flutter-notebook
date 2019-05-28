ab7b4d884264e4f0019980e3e02609b2d2dd1df1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-05-29 00:42:31 +0900

    continue changing

--- layout_practice01/lib/main.dart
+++ layout_practice01/lib/main.dart
@@ -5,6 +5,19 @@ void main() => runApp(myApp());
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget upperSection = Container(
+      child: Row(
+        children: <Widget>[
+      Expanded(
+        flex: 5,
+       child:   Image.asset('images/picture.jpg',
+              width: 200, height: 300, fit: BoxFit.fitWidth)),
+          Expanded(flex: 5, child: _starColumns())
+
+        ],
+      ),
+    );
+
     // TODO: implement build
     return MaterialApp(
       title: "layout practice",
@@ -13,45 +26,45 @@ class myApp extends StatelessWidget {
           title: Text("Layout02 Practice"),
         ),
         body: Column(
-          children: <Widget>[
-//            Image.asset(
-//              'images/picture.jpg',
-//              width: 600,
-//              height: 300,
-//              fit: BoxFit.cover,
-//            ),
-//            titleSection,
-//            detailSection
-            upperSection,
-//              downSection
-          ],
+          children: <Widget>[upperSection, downSection],
         ),
       ),
     );
   }
 
-  Widget upperSection = Container(
-    child: Row(
-      children: <Widget>[
-        Image.asset('images/picture.jpg',
-            width: 200, height: 300, fit: BoxFit.cover),
-        Icon(
-          Icons.star,
-          color: Colors.yellow,
-          size: 150.0,
-        ),
-      ],
-    ),
-  );
+  Widget _starColumns() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _starColumn("Tim"),
+          _starColumn("Alan"),
+          _starColumn("Riz"),
+          _starColumn("John"),
+          _starColumn("Jake")
+        ],
+      ),
+    );
+  }
 
-  Widget titleSection = Container(
-    padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-    child: Text(
-      'A Empty City',
-      style: TextStyle(color: Colors.grey[500], fontSize: 30.0),
-    ),
-  );
-  Widget detailSection = Container(
+  Widget _starColumn(String name) {
+    return Column(
+      children: <Widget>[
+        Center(
+          child: Text(
+            name,
+            style: TextStyle(
+                fontSize: 30.0
+            ),
+          ),
+
+        )
+      ],
+    );
+  }
+
+  Widget downSection = Container(
     padding: const EdgeInsets.all(10),
     child: Text(
       'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
