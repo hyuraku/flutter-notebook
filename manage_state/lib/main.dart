4b71f4fb87841d7cb302be3248afc66d94aa1d00
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-06-01 12:56:57 +0900

    setState in a class

--- manage_state/lib/main.dart
+++ manage_state/lib/main.dart
@@ -13,9 +13,51 @@ class myApp extends StatelessWidget {
           title: Text('Manage Demo'),
         ),
         body: Center(
-          child: Text('Start!'),
+          child: TapboxA(),
         ),
       ),
     );
   }
 }
+
+class TapboxA extends StatefulWidget{
+  TapboxA({Key key}): super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA>{
+  bool _active = false;
+
+  void _handleTap(){
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active!' : 'Inactive...',
+            style: TextStyle(
+              fontSize: 32.0,
+              color: Colors.white
+            ),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600]
+        ),
+      ),
+    );
+  }
+
+}
+
