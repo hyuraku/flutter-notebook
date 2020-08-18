327eda626071cb69d130085ca385c871dd6fafd0
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-08-18 22:09:17 +0900

    add two methods

--- json_image/lib/main.dart
+++ json_image/lib/main.dart
@@ -1,4 +1,39 @@
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+Future<List<Photo>> fetchPhotos(http.Client client) async{
+  final response = await client.get('https://jsonplaceholder.typicode.com/photos');
+
+  return compute (parsePhotos, response.body);
+}
+
+List<Photo> parsePhotos(String responseBody){
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class Photo{
+  final int id;
+  final String title;
+  final String thumbnailUrl;
+
+  Photo({this.id, this.title, this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json){
+    return Photo(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String
+    );
+  }
+}
 
 void main() {
   runApp(MyApp());
@@ -45,14 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Photo{
-  final int id;
-  final String title;
-  final String thumbnailUrl;
 
-  Photo({this.id, this.title, this.thumbnailUrl});
 
-  // factory Photo.fromJson(Map<String dynamic> json){
 
-  // }
-}
